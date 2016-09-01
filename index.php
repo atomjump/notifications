bfc178232eb36e85e9f785c528f34aa4fe7baba3
AtomJump <peter@atomjump.com>
2016-09-01 15:59:23 +1200

    test notification

---
+++ index.php
@@ -0,0 +1,75 @@
+// Payload data you want to send to Android device(s)
+// (it will be accessible via intent extras)    
+$data = array('message' => 'Hello World!');
+
+// The recipient registration tokens for this notification
+// https://developer.android.com/google/gcm/    
+$ids = array('abc', 'def');
+
+// Send push notification via Google Cloud Messaging
+sendPushNotification($data, $ids);
+
+function sendPushNotification($data, $ids)
+{
+    // Insert real GCM API key from the Google APIs Console
+    // https://code.google.com/apis/console/        
+    $apiKey = 'abc';
+
+    // Set POST request body
+    $post = array(
+                    'registration_ids'  => $ids,
+                    'data'              => $data,
+                 );
+
+    // Set CURL request headers 
+    $headers = array( 
+                        'Authorization: key=' . $apiKey,
+                        'Content-Type: application/json'
+                    );
+
+    // Initialize curl handle       
+    $ch = curl_init();
+
+    // Set URL to GCM push endpoint     
+    curl_setopt($ch, CURLOPT_URL, 'https://gcm-http.googleapis.com/gcm/send');
+
+    // Set request method to POST       
+    curl_setopt($ch, CURLOPT_POST, true);
+
+    // Set custom request headers       
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    // Get the response back as string instead of printing it       
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Set JSON post data
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+
+    // Actually send the request    
+    $result = curl_exec($ch);
+
+    // Handle errors
+    if (curl_errno($ch))
+    {
+        echo 'GCM error: ' . curl_error($ch);
+    }
+
+    // Close curl handle
+    curl_close($ch);
+
+    // Debug GCM response       
+    echo $result;
+}
+
+/*
+https://gcm-http.googleapis.com/gcm/send
+Content-Type:application/json
+Authorization:key=AIzaSyZ-1u...0GBYzPu7Udno5aA
+
+{ "data": {
+    "score": "5x1",
+    "time": "15:10"
+  },
+  "to" : "bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1..."
+}
+*/
