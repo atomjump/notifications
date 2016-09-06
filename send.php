cb16e7500f1b1563a67633a473034b6ae1d8eb57
AtomJump <peter@atomjump.com>
2016-09-07 08:47:05 +1200

    sending from the background

---
+++ send.php
@@ -0,0 +1,65 @@
+<?php
+
+       public function sendPushNotification($data, $ids)
+		{
+			// Insert real GCM API key from the Google APIs Console
+			// https://code.google.com/apis/console/        
+			$apiKey = $this->notifications_config['apiKey'];
+
+			// Set POST request body
+			$post = array(
+							'registration_ids'  => $ids,
+							'data'              => $data,
+						 );
+
+			// Set CURL request headers 
+			$headers = array( 
+								'Authorization: key=' . $apiKey,
+								'Content-Type: application/json'
+							);
+
+			// Initialize curl handle       
+			$ch = curl_init();
+
+			// Set URL to GCM push endpoint     
+			curl_setopt($ch, CURLOPT_URL, 'https://android.googleapis.com/gcm/send');
+
+			// Set request method to POST       
+			curl_setopt($ch, CURLOPT_POST, true);
+
+			// Set custom request headers       
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+			// Get the response back as string instead of printing it       
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+			// Set JSON post data
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+
+			// Actually send the request    
+			$result = curl_exec($ch);
+
+			// Handle errors
+			if (curl_errno($ch))
+			{
+				error_log('GCM error: ' . curl_error($ch));
+			}
+
+			// Close curl handle
+			curl_close($ch);
+
+			// Debug GCM response       
+			error_log($result);
+			
+			return;
+		}
+
+    }
+    
+  
+  	sendPushNotification(json_decode($argv[1]), json_decode($argv[2]));
+  
+  
+    
+    
+?>
