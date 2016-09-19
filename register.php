17ed02d939d2155039bbff5fd0a9088fbae23a5a
AtomJump <peter@atomjump.com>
2016-09-19 12:19:15 +1200

    first attempt at ios msgs

--- register.php
+++ register.php
@@ -45,7 +45,7 @@
 		$device_type = "Android";			//Default to Android if unknown
 	}
 	
-	$sql = "var_notification_id = " . $notification_id . ", var_device_type = " . $device_type . " WHERE int_user_id = " . $_REQUEST['userid'];
+	$sql = "var_notification_id = " . $notification_id . ", var_device_type = '" . $device_type . "' WHERE int_user_id = " . $_REQUEST['userid'];
 	$api->db_update("tbl_user", $sql);
 
 	echo "Updated";
