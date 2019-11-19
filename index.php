c6a55eea67195555a91bd579a4434f8437edfcd4
AtomJump <peter@atomjump.com>
2019-11-20 09:48:38 +1300

    with private server

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
 							if (lastChar != '/') {         // If the last character is not a slash
 							   url = url + '/';            // Append a slash to it.
 							}
-                    		var myText = "<span style='color: #BBB;'>Note: Please use the 'Private Server'</span></br><span style='color: #AAA;'>" + url + "</span><br/><span style='color: #BBB;'>in your app login screen.</span>";
+                    		var myText = "<span style='color: #BBB;'>Note: Please set the 'Private Server' in your app login screen as:</span></br><span style='color: #AAA;'>" + url + "</span>";
                     		$('#show-server-app-link').html(myText);
                     		$('#show-server-app-link').show();
                     	}
