433c3563d30ae4dd403f63438682a1ac66008172
AtomJump <peter@atomjump.com>
2021-02-19 11:50:56 +1300

    checking if server has pull

---
+++ check-pull.php
@@ -0,0 +1,48 @@
+<?php
+
+	//Checks if this server has pull notifications
+
+
+	function trim_trailing_slash_local($str) {
+        return rtrim($str, "/");
+    }
+    
+    function add_trailing_slash_local($str) {
+        //Remove and then add
+        return rtrim($str, "/") . '/';
+    }
+
+
+	//Called by the AtomJump messaging app to register this particular user's phone
+	
+	if(!isset($notifications_config)) {
+        //Get global plugin config - but only once
+		$data = file_get_contents (dirname(__FILE__) . "/config/config.json");
+        if($data) {
+            $notifications_config = json_decode($data, true);
+            if(!isset($notifications_config)) {
+                echo "Error: notifications config/config.json is not valid JSON.";
+                exit(0);
+            }
+     
+        } else {
+            echo "Error: Missing config/config.json in notifications plugin.";
+            exit(0);
+     
+        }
+  
+  
+    }
+ 
+	$start_path = add_trailing_slash_local($notifications_config['serverPath']);
+	$staging = $notifications_config['staging'];
+	
+	$notify = false;
+	
+	if(($notifications_config['atomjumpNotifications']) && (isset($notifications_config['atomjumpNotifications']['use'])) {
+		echo "true";
+	} else {
+		echo "false";
+	}
+
+?>
