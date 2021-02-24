72598a2efb85c9a9efa30c12ba15d5e67be16eb9
AtomJump <peter@atomjump.com>
2021-02-24 13:06:29 +1300

    genid with cors accept heading

--- genid.php
+++ genid.php
@@ -22,6 +22,45 @@ CREATE TABLE `tbl_notification_pairing` (
 ) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=latin1;
 */
 
+/**
+ *  An example CORS-compliant method.  It will allow any GET, POST, or OPTIONS requests from any
+ *  origin.
+ *
+ *  In a production environment, you probably want to be more restrictive, but this gives you
+ *  the general idea of what is involved.  For the nitty-gritty low-down, read:
+ *
+ *  - https://developer.mozilla.org/en/HTTP_access_control
+ *  - https://fetch.spec.whatwg.org/#http-cors-protocol
+ *
+ */
+function cors() {
+    
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    
+        exit(0);
+    }
+    
+    echo "You have CORS!";
+}
+
+
 function trim_trailing_slash_local($str) {
 	return rtrim($str, "/");
 }
@@ -177,7 +216,7 @@ function preserve_qs() {
     }
 
 
-
+ 	cors();
     $result = dbquery($sql)  or die("Unable to execute query $sql " . dberror());
     echo $passcode . " " . $guid . " " . $proxy . " " . $country_used;
  }
