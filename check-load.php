b4b74d71b5200e83983c27aed2b1ecf0d2ad0bee
AtomJump <peter@atomjump.com>
2021-02-26 04:29:10 +1300

    use nodejs config

--- check-load.php
+++ check-load.php
@@ -2,7 +2,7 @@
 
 	/*
 	
-		TODO: this will be a cron script run once a day or so.
+		This is a cron script run once a day or so.
 		
 		It will check through the pool of MedImage servers in config/config.json atomjumpNotifications.serverPool
 		and for each one get the load using the URL   medimageserver.url/load/
@@ -17,5 +17,53 @@
 	
 	*/
 
+	function trim_trailing_slash_local($str) {
+		return rtrim($str, "/");
+	}
 
+	function add_trailing_slash_local($str) {
+		//Remove and then add
+		return rtrim($str, "/") . '/';
+	}
+
+	if(!isset($notifications_config)) {
+		//Get global plugin config - but only once
+		$data = file_get_contents (dirname(__FILE__) . "/config/config.json");
+		if($data) {
+			$notifications_config = json_decode($data, true);
+			if(!isset($notifications_config)) {
+				echo "Error: notifications config/config.json is not valid JSON.";
+				exit(0);
+			}
+ 
+		} else {
+			echo "Error: Missing config/config.json in notifications plugin.";
+			exit(0);
+ 
+		}
+
+	}
+
+
+
+	$start_path = add_trailing_slash_local($notifications_config['serverPath']);
+
+	$staging = $notifications_config['staging'];
+	$notify = false;
+	include_once($start_path . 'config/db_connect.php');
+	
+	echo "Checking server pool..\n";
+  	if(isset($notifications_config['atomjumpNotifications']) 
+  		&& isset($notifications_config['atomjumpNotifications']['serverPool'])) {
+	
+		foreach ($notifications_config['atomjumpNotifications']['serverPool'] as $country) {
+				if ($country->_visible == 1) {
+					echo $country;
+				}
+		}
+	}
+	
+		
+	
+		
 ?>
