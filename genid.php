271f044520efa59c29dc0bddc87365e1d066ffd8
AtomJump <peter@atomjump.com>
2021-02-26 08:30:28 +1300

    least load option

--- genid.php
+++ genid.php
@@ -119,7 +119,7 @@ function get_least_load($server_pool, $country_code) {
 		$load_file_str = file_get_contents($load_file);
 		$load = json_decode($load_file_str);
 		if(isset($load)) {
-			$server_sorted_pool = $load['atomjumpNotifications']['countryServerResidingIn'][$country_code];
+			$server_sorted_pool = $load->atomjumpNotifications->countryServerResidingIn[$country_code];
 			if((isset($server_sorted_pool)) &&
 				(isset($server_sorted_pool[0]['url'])) ) {
 				return $server_sorted_pool[0]['url'];			
