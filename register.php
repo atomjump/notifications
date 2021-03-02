e55447a4a6d10cf3d52c7ef09890b25718557bde
AtomJump <peter@atomjump.com>
2021-03-02 21:56:44 +1300

    Remove subdomain mentions

--- register.php
+++ register.php
@@ -199,9 +199,12 @@
 	
 	$subdomain = check_subdomain();
 	$webroot = trim_trailing_slash($cnf['webRoot']);
-	error_log($webroot);
-	if($subdomain) {
-		$webroot = trim_trailing_slash(str_replace('[subdomain]',$subdomain . "." ,$cnf['webRoot']));
+	error_log("Root:" . $webroot . "  subdomain:" . $subdomain);
+	
+	if(isset($subdomain)) {
+		$replace_with = $subdomain . ".";
+		error_log("Replace with:" . $replace_with);
+		$webroot = trim_trailing_slash(str_replace("[subdomain]", $replace_with,$webroot));
 	}
 	
 	
