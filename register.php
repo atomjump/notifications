b7eed9d3cd55247ad63b31dea2e1e9010181d0f6
AtomJump <peter@atomjump.com>
2021-03-12 10:56:02 +1300

    signup page

--- register.php
+++ register.php
@@ -525,7 +525,7 @@
 				 				 <input type="hidden" name="date-owner-start" value="<?php echo $date_start ?>">
 				 				 <input type="hidden" id="email-modified" name="email_modified" value="false">
 				 				 <?php $sh->call_plugins_settings(null); //User added plugins here ?>								
-				 				 
+				 				 <br/><br/>
 								 <div class="form-group">
 		 									<div><?php echo $msg['msgs'][$lang]['yourEmail'] ?></div>
 						  					<input oninput="if(this.value.length > 0) { $('#email-modified').val('true'); $('#save-button').html('<?php if($msg['msgs'][$lang]['subscribeSettingsButton']) {
