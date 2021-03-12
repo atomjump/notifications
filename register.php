3648f29f9b5b54bfdb604d7b4b1ade7d9d116d16
AtomJump <peter@atomjump.com>
2021-03-12 15:16:03 +1300

    signup page

--- register.php
+++ register.php
@@ -136,7 +136,7 @@
 		$main_message = $notifications_config['msgs'][$lang]['notLoggedIn'];
 		$first_button = "#comment-open-Setup";
 		$first_button_wording = $notifications_config['msgs'][$lang]['openSetup'];
-		$follow_on_link = "#comment-open-Setup";
+		//$follow_on_link = "#comment-open-Setup";
 		$second_button = "";	//"javascript: window.close()";
 		$second_button_wording = ""; 
 		$center = "left";   //$notifications_config['msgs'][$lang]['returnToApp'];
