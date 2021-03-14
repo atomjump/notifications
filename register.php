1f2911e3a9ee460c9eb5f4d75cbe3339ce57705d
AtomJump <peter@atomjump.com>
2021-03-15 09:38:26 +1300

    keep sign in button

--- register.php
+++ register.php
@@ -707,7 +707,7 @@
 		 									<div style="display: none; color: red;" id="comment-messages"></div>
 								</div>
 								<br/>
-							 <button id="sign-and-pair-button" type="submit" onclick="" class="btn btn-primary" style="margin-bottom:3px;"><?php echo $notifications_config['msgs'][$lang]['signUp']; ?></button>
+							 <button id="sign-and-pair-button" type="submit" onclick="" class="btn btn-primary" style="margin-bottom:3px;"><?php echo $notifications_config['msgs'][$lang]['signUp']; ?></button><br/>
 							 <button style="display: none;" id="pair-again-button" onclick="" class="btn btn-primary btn-lg" style="margin-bottom:3px;"><?php echo $notifications_config['msgs'][$lang]['completePairing']; ?></button>
 							<br/>
 							<br/>
